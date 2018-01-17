95da9c0d337cef68d34121003076eed579ac56bd
Spencer Taylor <spencertaylor808@gmail.com>
2018-01-17 21:31:38 +0000

    Add 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints string to command line
+*/
+void main(void)
+{
+	printf("Size of a char:%zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)", sizeof(long));
+	printf("Size of a long long int: &zu byte(s)", sizeof(long long));
+	printf("Size of a float: %zu byte(s)", sizeof(float));
+}
