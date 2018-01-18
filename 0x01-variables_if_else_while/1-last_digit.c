8a9e384150db17f1507e75f05da7c7900ebe6d4b
Spencer Taylor <spencertaylor808@gmail.com>
2018-01-18 21:17:39 +0000

    Update 1-last_digit.c
    Added new line to each if else printf statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,10 +17,10 @@ int main(void)
 	last_digit = n % 10;
 	printf("%s%d%s%d ", "Last digit of ", n, " is ", last_digit);
 	if (last_digit > 5)
-		printf("%s", "and is greater than 5");
+		printf("%s", "and is greater than 5\n");
 	else if (last_digit == 0)
-		printf("%s", "and is 0");
+		printf("%s", "and is 0\n");
 	else if (last_digit < 6 && last_digit != 0)
-		printf("%s", "and is less than 6 and not 0");
+		printf("%s", "and is less than 6 and not 0\n");
 	return (0);
 }
