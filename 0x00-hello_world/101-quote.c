d25720cb964fbeb4ea3a2489e3594f608a1fd120
Spencer Taylor <spencertaylor808@gmail.com>
2018-01-19 23:06:31 +0000

    Add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints string to standard error
+ * Return: Return 0
+*/
+int main(void)
+{
+	fprintf(stderr, "%s",
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
