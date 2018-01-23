3868a5e6f2958dc1c2dcb69addb7e7ec65bd0ba9
Spencer Taylor <spencertaylor808@gmail.com>
2018-01-23 04:29:47 +0000

    Update 9-times_table
    Added two helper functions format_product and format_row to
    streamline the times table function.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,50 @@
 #include "holberton.h"
 
 /**
+ * format_product - spaces out products equally
+ * @x: first factor
+ * @y: second factor
+ */
+void format_product(int x, int y)
+{
+	int result = x * y;
+
+		if (y == 0)
+		{
+			_putchar(result + '0');
+		}
+		else if (result >= 10)
+		{
+			_putchar(' ');
+			_putchar(result / 10 + '0');
+			_putchar(result % 10 + '0');
+		}
+		else
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar((result) + '0');
+		}
+}
+
+/**
+ * format_row - formats a row of multiplication table
+ * @y: represents second factor in multiplication table
+ */
+void format_row(int y)
+{
+	if (y != 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
+/**
  * times_table - prints the 9 times table
  *
  * Return: no return value
@@ -8,38 +52,14 @@
 
 void times_table(void)
 {
-	int x, y, result;
+	int x, y;
+
 	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
-			result = x * y;
-			if (y == 0)
-			{
-				_putchar(result + '0');
-			}
-			else if (result >= 10)
-			{
-				_putchar(' ');
-				_putchar(result / 10 + '0');
-				_putchar(result % 10 + '0');
-			}
-			else
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((result) + '0');
-			}
-
-			if (y != 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar('\n');
-			}
+			format_product(x, y);
+			format_row(y);
 		}
 	}
 }
