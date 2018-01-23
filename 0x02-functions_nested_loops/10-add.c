a23b2ff8d390bbac1483f0b6e47ac68737a47c13
Spencer Taylor <spencertaylor808@gmail.com>
2018-01-23 00:29:57 +0000

    Add 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers together
+ * @a: first number to be added
+ * @b: second number to be added
+ *
+ * Return: returns the result of a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
