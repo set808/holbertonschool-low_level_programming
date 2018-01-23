995eaaed645dee6601cabd8eae89fde9e670bc35
Spencer Taylor <spencertaylor808@gmail.com>
2018-01-23 21:44:57 +0000

    Add 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ *
+ * Return: Always 0;
+ */
+
+int  main(void)
+{
+	int i;
+	float x, y, z;
+
+	x = 1;
+	y = 2;
+
+	printf("%.0f, ", x);
+	printf("%.0f", y);
+	for (i = 0; i < 96; i++)
+	{
+		z = x + y;
+		printf(", %.0f", z);
+		x = y;
+		y = z;
+	}
+	putchar('\n');
+	return (0);
+}
