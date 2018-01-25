c8f98ebedd1f776d4bbdd200728500317184e8f7
Spencer Taylor <spencertaylor808@gmail.com>
2018-01-25 17:40:31 +0000

    Add 2-mul.c
    Also had to update holberton.h because name for mul function was
    wrong

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - Multiplies 2 integers
+ * @a: first number to be multiplied
+ * @b: second number to be multiplied
+ *
+ * Return: Returns the product of a and b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
