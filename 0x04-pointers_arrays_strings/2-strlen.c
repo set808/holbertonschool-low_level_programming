ab8fef027ea8c623b3beb8018c40150268854bf2
Spencer Taylor <spencertaylor808@gmail.com>
2018-01-29 20:57:44 +0000

    Add 2-strlen.c

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: string we find the length of
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	char *length;
+
+	*length = s;
+
+	while (*length != '\0')
+		length++;
+	return (length - s);
+}
