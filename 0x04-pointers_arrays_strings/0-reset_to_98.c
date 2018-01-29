e70d2f89b4548e4bdb60a727914b3754d6dc3bd9
Spencer Taylor <spencertaylor808@gmail.com>
2018-01-29 20:54:22 +0000

    Create new project directory
    Create new holberton.h header file. Add 0-reset_to_98.c to repo.

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - takes a pointer and updates the value it points to, to 98
+ * @n: pointer that points to value to be updated
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
