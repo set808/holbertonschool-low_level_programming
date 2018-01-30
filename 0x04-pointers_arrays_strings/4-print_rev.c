3a275a865ce31dbfc45354752fc2507506a796a5
Spencer Taylor <spencertaylor808@gmail.com>
2018-01-30 21:44:03 +0000

    Update 4-print_rev.c

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -10,8 +10,9 @@ void print_rev(char *s)
 	int x;
 	char *temp = s;
 
-	for (x = 0; *(temp + 1) != '\0'; x++)
+	for (x = 0; temp != '\0'; x++)
 		temp++;
+	temp--;
 	while (x >= 0)
 	{
 		_putchar(*temp);
