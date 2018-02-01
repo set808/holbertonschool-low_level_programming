9d0c4b68d7dc729879268a0203ccab44914f28e2
Spencer Taylor <spencertaylor808@gmail.com>
2018-02-01 20:48:05 +0000

    Add 1-strncat.c

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string to be appended to
+ * @src: source string to append to destination
+ * @n: number of bytes of src to be used
+ *
+ * Return: returns a pointer to concatenated string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int x, y;
+
+	/* Find length of first string */
+	for (x = 0; *(dest + x) != '\0'; x++)
+		;
+	/* Append src to dest and check if byte is empty*/
+	for (y = 0; y < n && *(src + y) != '\0'; y++)
+	{
+		*(dest + x) = *(src + y);
+		x++;
+	}
+	/* Add null terminator to dest */
+	*(dest + x) = '\0';
+	return (dest);
+
+}
