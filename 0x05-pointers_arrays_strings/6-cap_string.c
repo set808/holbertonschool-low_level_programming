9548d13ccdfc1cb540e10a99abffb974bcb34640
Spencer Taylor <spencertaylor808@gmail.com>
2018-02-03 22:48:48 +0000

    Add 6-cap_string.c

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * is_sep - checks if character is a seperator
+ * @c: character to be checked
+ *
+ * Return: if seperator return 1. Otherwise return 0;
+ */
+
+int is_sep(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n' || c == ',' ||
+	    c == ';' ||	c == '.' || c == '!' || c == '?' ||
+	    c == '"' || c == '(' || c == ')' || c == '{' ||
+	    c == '}')
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * cap_string - capitalizes all words in a string
+ * @s: string to be capitalized
+ *
+ * Return: returns capitalized strings
+ */
+
+char *cap_string(char *s)
+{
+	int x, upper;
+
+	upper = -32;
+	for (x = 0; *(s + x) != '\0'; x++)
+	{
+		if (*(s + x) >= 'a' && *(s + x) <= 'z')
+			if (*(s + x) == *s || is_sep(*(s + x - 1)))
+			    *(s + x) += upper;
+	}
+	return (s);
+}
