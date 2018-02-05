11fbdd161088e178f7961145ceda61511e40f046
Spencer Taylor <spencertaylor808@gmail.com>
2018-02-05 17:52:29 +0000

    Add 2-strchr.c

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to be searched
+ * @c: character to be found
+ *
+ * Return: returns a pointer to the first occurence of character c.
+ * returns NULL if the character isn't found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int x;
+	char *found = 0;
+
+	for (x = 0; *(s + x) != '\0'; x++)
+	{
+		if (*(s + x) == c)
+		{
+			found = &*(s + x);
+			break;
+		}
+	}
+	return (found);
+}
