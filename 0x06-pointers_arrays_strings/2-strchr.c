5adbe6ccea5dabf9e0b33a4678d560490ef84122
Spencer Taylor <spencertaylor808@gmail.com>
2018-02-05 21:19:03 +0000

    Update 2-strchr.c
    Accounted for case where c could be '\0'.

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -14,7 +14,7 @@ char *_strchr(char *s, char c)
 	int x;
 	char *found = 0;
 
-	for (x = 0; *(s + x + 1) != '\0'; x++)
+	for (x = 0; *(s + x) != '\0'; x++)
 	{
 		if (*(s + x) == c)
 		{
@@ -22,5 +22,8 @@ char *_strchr(char *s, char c)
 			break;
 		}
 	}
-	return (found);
+	if (c == '\0')
+		return (&*(s + x));
+	else
+		return (found);
 }
