5425e989b1ae93e8b34a3e0cda3415ed44086613
Spencer Taylor <spencertaylor808@gmail.com>
2018-02-05 18:34:25 +0000

    Add 4-strpbrk.c

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _stpbrk - scans a string for any byte in a set of bytes
+ * @s: string to be scanned
+ * @accept: set of bytes to be matched
+ *
+ * Return: returns a pointer to the first match
+ */
+
+char  *_strpbrk(char *s, char *accept)
+{
+	unsigned int x, y;
+	char *found = 0;
+
+	for (x = 0; *(s + x) != '\0'; x++)
+	{
+		for (y = 0; *(accept + y) != '\0'; y++)
+		{
+			if (*(s + x) == *(accept + y))
+			{
+				found = &*(s + x);
+				return (found);
+			}
+		}
+	}
+	return (found);
+}
