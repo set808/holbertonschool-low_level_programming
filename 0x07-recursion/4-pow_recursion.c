4dda02c21813395542b781af9104f19171ff9fa6
Spencer Taylor <spencertaylor808@gmail.com>
2018-02-07 20:12:35 +0000

    Add 4-pow_recursion.c

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - returns the value of x to the power of y
+ * @x: base number
+ * @y: exponent
+ *
+ * Return: returns the the value of x to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
