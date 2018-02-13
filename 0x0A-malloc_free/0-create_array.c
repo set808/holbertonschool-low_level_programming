72dec86068f195fadc2224bac9842ee5218abacf
Spencer Taylor <spencertaylor808@gmail.com>
2018-02-13 21:37:21 +0000

    Update 0-create_array.c

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -18,10 +18,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	s = malloc(sizeof(char) * size);
+	if (s == NULL)
+		return (NULL);
 
 	for (x = 0; x < size ; x++)
 		s[x] = c;
-	if (s == NULL)
-		return (NULL);
 	return (s);
 }
