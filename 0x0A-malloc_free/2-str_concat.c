443cbbb5792efdc860b72965492f6545d29caacf
Spencer Taylor <spencertaylor808@gmail.com>
2018-02-13 23:28:25 +0000

    Update 2-str_concat.c

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -35,7 +35,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	s = malloc(sizeof(_strlen(s1)) + sizeof(_strlen(s2)));
+	s = malloc(sizeof(char) * _strlen(s2) + 1);
 	if (s == NULL)
 		return (NULL);
 
