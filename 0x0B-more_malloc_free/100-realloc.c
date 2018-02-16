ce78041c983a9c16ff3b11fd38a9023bf086092b
Spencer Taylor <spencertaylor808@gmail.com>
2018-02-16 03:46:28 +0000

    Update 100-realloc.c

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -11,6 +11,11 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	if (new_size <= old_size)
 		return (ptr);
 	if (ptr == NULL)
@@ -18,13 +23,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		ptr = malloc(new_size);
 		return (ptr);
 	}
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
 
 	free(ptr);
 	ptr = malloc(new_size);
-	return(ptr);
+	return (ptr);
 }
