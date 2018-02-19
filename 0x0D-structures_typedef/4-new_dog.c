ced6ca96627b2ebc43c3ffab266be3a6f21f700d
Spencer Taylor <spencertaylor808@gmail.com>
2018-02-19 23:17:17 +0000

    Update 4-new_dog.c

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,6 +1,47 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+ * _strlen - returns the length of a string
+ * @s: string we find the length of
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int x = 0;
+
+	while (*(s + x) != '\0')
+		x++;
+	return (x);
+}
+
+/**
+ * _strdup - returns a pointer to newly allocated space in memory that contains
+ * a copy of the string given as a parameter
+ * @str: string to copy
+ *
+ * Return: returns a pointer to the copied string
+ */
+
+char *_strdup(char *str)
+{
+	int x;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	s = malloc(sizeof(char) * (_strlen(str) + 1));
+	if (s == NULL)
+		return (NULL);
+	for (x = 0; *(str + x) != '\0'; x++)
+		*(s + x) = *(str + x);
+	*(s + x) = '\0';
+	return (s);
+}
+
+/**
  * new_dog - creates  a new dog
  * @name: name of dog
  * @age: age of dog
@@ -12,7 +53,6 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	int x;
 	char *n, *o;
 
 	if (name == NULL || owner == NULL)
@@ -21,29 +61,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	n = malloc(sizeof(name));
+	n = _strdup(name);
 	if (n == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	o = malloc(sizeof(owner));
-	if (o == NULL)
+	o = _strdup(owner);
+	if (0 == NULL)
 	{
-		free(n);
 		free(new_dog);
 		return (NULL);
 	}
 
-	for (x = 0; name[x] != '\0'; x++)
-		n[x] = name[x];
-	n[x] = '\0';
-	for (x = 0; owner[x] != '\0'; x++)
-		o[x] = owner[x];
-	o[x] = '\0';
+
 
 	new_dog->name = n;
 	new_dog->age = age;
-	new_dog->owner = 0;
+	new_dog->owner = o;
 	return (new_dog);
 }
