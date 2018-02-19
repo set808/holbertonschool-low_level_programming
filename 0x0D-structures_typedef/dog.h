9e78c9d7c9b160e88f5e0613e0a6e758973eb790
Spencer Taylor <spencertaylor808@gmail.com>
2018-02-19 22:20:00 +0000

    Add dog.h

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HI
+#define HI
+/**
+ * struct dog - defines a structure for dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* HI */
