072f55a70fe64deda57551b97355617d5465e4fb
Spencer Taylor <spencertaylor808@gmail.com>
2018-02-19 23:43:11 +0000

    Tidy 4-new_dog.c

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -34,6 +34,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (name == NULL || owner == NULL)
 		return (NULL);
 
+	/* memory allocation for pointers */
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
@@ -43,21 +44,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog);
 		return (NULL);
 	}
-	for (x = 0; name[x] != '\0'; x++)
-		n[x] = name[x];
-	n[x] = '\0';
-	new_dog->name = n;
-	new_dog->age = age;
 	o = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if (o == NULL)
 	{
-		free(new_dog->name);
+		free(n);
 		free(new_dog);
 		return (NULL);
 	}
+
+	/* copy string arguments into new variables */
+	for (x = 0; name[x] != '\0'; x++)
+		n[x] = name[x];
+	n[x] = '\0';
 	for (x = 0; owner[x] != '\0'; x++)
 		o[x] = owner[x];
 	o[x] = '\0';
+
+	/*initializing new dog */
+	new_dog->name = n;
+	new_dog->age = age;
 	new_dog->owner = o;
 
 	return (new_dog);
