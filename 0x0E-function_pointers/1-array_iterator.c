9120534d5604342aeb181f3caf7d60cc4b44487b
Spencer Taylor <spencertaylor808@gmail.com>
2018-02-21 20:17:26 +0000

    Update 1-array_iterator.c

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int x;
 
-	if (array == NULL)
+	if (array == NULL || action == NULL)
 		return;
 	for (x = 0; x < size; x++)
 		action(array[x]);
