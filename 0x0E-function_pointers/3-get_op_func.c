0593de1b4656ad5a509925133a95d966ea487bd8
Spencer Taylor <spencertaylor808@gmail.com>
2018-02-21 23:22:00 +0000

    Remove else statement from 3-get_op_func.c

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -23,10 +23,9 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (strcmp(ops[i].op, s))
-			i++;
-		else
+		if (*s == *(ops[i].op) && s[1] == '\0')
 			return (ops[i].f);
+		i++;
 	}
 	printf("Error\n");
 	exit(99);
