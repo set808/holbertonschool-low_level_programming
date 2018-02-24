89e8fcf5e27888fe05df4420bdf8be53544d4328
Spencer Taylor <spencertaylor808@gmail.com>
2018-02-24 20:47:18 +0000

    Create print strings function

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_strings - prints strings, followed by a newline
+ * @separator: string printed between strings
+ * @n: number f arguments
+ *
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *ptr;
+
+	if (n == 0)
+		return;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		ptr = va_arg(args, char *);
+		if (ptr == NULL)
+			printf("(nil)");
+		else
+			printf("%s", ptr);
+		if (seperator != NULL && (i + 1) != n)
+			printf("%s", seperator);
+	}
+	printf("%c", '\n');
+	va_end(args);
+}
