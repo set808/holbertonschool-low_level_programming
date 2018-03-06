1bc53229ca45e9ce0a54fbaea5a4f8095b0a07d3
Spencer Taylor <spencertaylor808@gmail.com>
2018-03-06 18:37:00 +0000

    Add check for linked list NULL

--- 0x12-more_singly_linked_lists/6-pop_listint.c
+++ 0x12-more_singly_linked_lists/6-pop_listint.c
@@ -11,7 +11,7 @@ int pop_listint(listint_t **head)
 	int number;
 	listint_t *tmp;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 	number = (*head)->n;
 	tmp = (*head)->next;
