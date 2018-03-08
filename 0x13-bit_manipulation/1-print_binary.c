33232ab55f0b0d4cd0368a738044057e60f56b6f
Spencer Taylor <spencertaylor808@gmail.com>
2018-03-08 22:42:43 +0000

    Create decimal to binary function

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the number
+ *
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned int token;
+	unsigned long int mask;
+
+	token = 0;
+	mask = 4294967296;
+	if (n == 0)
+		_putchar('0');
+
+	while (mask > 0)
+	{
+
+		if ((n & mask) == 0 && token != 1)
+		{
+			mask = mask >> 1;
+			continue;
+		}
+
+		if ((n & mask) != 0)
+		{
+			token = 1;
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+		mask = mask >> 1;
+	}
+}
