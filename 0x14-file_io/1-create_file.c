f7c8be4728ace7f9fb1688c167a254e4eac8d62e
Spencer Taylor <spencertaylor808@gmail.com>
2018-03-13 22:14:25 +0000

    Create function to create files

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+ * _strlen - returns the length of the string
+ * @s: string
+ *
+ * Return: returns the length of the string
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+		;
+	return (x);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to be created
+ * @text_content: null-termintated string to write to the file
+ *
+ * Return: returns 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_RDWR | O_CREAT, S_IRWXU);
+		if (fd == -1)
+			return (-1);
+		close(fd);
+		return (1);
+	}
+	fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	write(fd, text_content, _strlen(text_content));
+	close(fd);
+	return (1);
+}
