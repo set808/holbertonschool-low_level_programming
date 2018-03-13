8fb8c89fec1975fbdb9a0961d234ff39eafd2860
Spencer Taylor <spencertaylor808@gmail.com>
2018-03-13 22:21:19 +0000

    Make code truncate files

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
-	fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	write(fd, text_content, _strlen(text_content));
