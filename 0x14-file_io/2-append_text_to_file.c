f365a5000be8fa5b05e11a5083d4e54eb7567359
Spencer Taylor <spencertaylor808@gmail.com>
2018-03-13 22:50:18 +0000

    Fix Betty errors

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -26,16 +26,16 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 
 	if (filename == NULL)
-		return(-1);
+		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND, S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	if(text_content == NULL)
+	if (text_content == NULL)
 	{
 		close(fd);
-		return(1);
+		return (1);
 	}
 	write(fd, text_content, _strlen(text_content));
 	close(fd);
-	return(1);
+	return (1);
 }
