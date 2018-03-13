c9e5f395d119bdf629310d50063b401760c8c084
Spencer Taylor <spencertaylor808@gmail.com>
2018-03-13 22:18:29 +0000

    Change empty text_content code

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -28,13 +28,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
-	{
-		fd = open(filename, O_RDWR | O_CREAT, S_IRWXU);
-		if (fd == -1)
-			return (-1);
-		close(fd);
-		return (1);
-	}
+		text_content = "";
 	fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
