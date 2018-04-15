cfe17cd4e2fbbae38b35fbba17659c39f0aca0ce
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-15 18:07:48 +0000

    Refactor 7-insert_dnodeint.c

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,18 +15,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (h == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
-	new->n = n;
-
-	if (*h == NULL && idx == 0)
-	{
-		new->prev = NULL;
-		new->next = NULL;
-		*h = new;
-		return (new);
-	}
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
 	count = 0;
 	runner = *h;
@@ -37,9 +27,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		count++;
 		runner = runner->next;
 	}
-	new->next = runner->next;
-	new->next->prev = new;
-	runner->next = new;
-	new->prev = runner;
-	return (new);
+	if (runner->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+
+	else
+	{
+		new = malloc(sizeof(dlistint_t));
+		if (!new)
+			return (NULL);
+		new->n = n;
+		
+		new->next = runner->next;
+		new->next->prev = new;
+		runner->next = new;
+		new->prev = runner;
+	}
+		return (new);
 }
