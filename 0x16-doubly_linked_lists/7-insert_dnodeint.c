e977a3cfed98589abe4cfa999e80e6cf58ca9559
Spencer Taylor <spencertaylor808@gmail.com>
2018-04-15 18:09:16 +0000

    Correct betty error in 7-insert_dnodeint.c

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,7 +38,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (!new)
 			return (NULL);
 		new->n = n;
-		
 		new->next = runner->next;
 		new->next->prev = new;
 		runner->next = new;
