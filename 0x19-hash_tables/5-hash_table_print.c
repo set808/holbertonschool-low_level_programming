f272d665197fc3c2abfcb4e51688eba09fb01ee0
Spencer Taylor <spencertaylor808@gmail.com>
2018-05-11 19:18:36 +0000

    Create 5-hash_table_print.c

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table
+ *
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned int i, flag;
+	hash_node_t *runner;
+
+	i = 0;
+	flag = 1;
+	printf("{");
+	while (i < ht->size)
+	{
+		if (ht->array[i] == NULL)
+		{
+			i++;
+			continue;
+		}
+		if (flag == 0)
+			printf(", ");
+		flag = 0;
+		runner = ht->array[i];
+		while (runner)
+		{
+			printf("\'%s\': \'%s\'", runner->key, runner->value);
+			if (runner->next != NULL)
+				printf(", ");
+			runner = runner->next;
+		}
+		i++;
+	}
+	printf("}\n");
+}
