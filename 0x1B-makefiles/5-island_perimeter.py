a90fbcab3193d1e1a45d236258e89a280967c963
Spencer Taylor <spencertaylor808@gmail.com>
2018-06-16 06:04:04 +0000

    Rework 5-island_perimeter.py

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -16,8 +16,8 @@ def island_perimeter(grid):
         for y in range(len(grid[x])):
             if grid[x][y] == 1:
                 count += 1
-                if (x != 0 and y != len(grid[x]) - 1 and grid[x][y + 1] == 1):
+                if (y != len(grid[x]) - 1 and grid[x][y + 1] == 1):
                     adj += 1
-                if (y != 0 and x != len(grid) - 1 and grid[x + 1][y] == 1):
+                if (x != len(grid) - 1 and grid[x + 1][y] == 1):
                     adj += 1
     return 4 * count - 2 * adj
