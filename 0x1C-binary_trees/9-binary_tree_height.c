8313d7b9f8f711cccddea9dac9eac04078ab3f2b
Spencer Taylor <spencertaylor808@gmail.com>
2018-07-03 22:17:40 +0000

    Create 9-binary_tree_height.c

---
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+/*
+ * binary_tree_height - returns the height of the binary tree
+ * @node: the root of the binary tree
+ *
+ * Return: returns the height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (!tree)
+		return (0);
+
+	left_height = 0;
+	right_height = 0;
+	if (tree->left != NULL)
+	{
+		left_height++;
+		left_height += binary_tree_height(tree->left);
+
+	}
+	if (tree->right != NULL)
+	{
+		right_height++;
+		right_height += binary_tree_height(tree->right);
+	}
+
+	if (left_height > right_height)
+		return (left_height);
+	else
+		return (right_height);
+}
