31143033fe33df2519dc021470006c4e8ce1b6a9
Spencer Taylor <spencertaylor808@gmail.com>
2018-07-03 22:16:12 +0000

    Create 3-binary_tree_delete.c

---
+++ 0x1C-binary_trees/3-binary_tree_delete.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/*
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: the root of the binary tree
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	if  (tree->left != NULL)
+		binary_tree_delete(tree->left);
+	if (tree->right != NULL)
+		binary_tree_delete(tree->right);
+	free(tree);
+}
