4dc6a2ea82d696d06572f0be81d9ff24954bc003
Spencer Taylor <spencertaylor808@gmail.com>
2018-07-05 18:03:13 +0000

    Remove root check

--- 0x1C-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1C-binary_trees/4-binary_tree_is_leaf.c
@@ -9,8 +9,6 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (!node)
 		return (0);
-	if (node->parent == NULL)
-		return (0);
 	if (node->left != NULL || node->right != NULL)
 		return (0);
 	return (1);
