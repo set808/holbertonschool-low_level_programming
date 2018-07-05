1c1a610ba22871209c210037138e402d383eaf27
Spencer Taylor <spencertaylor808@gmail.com>
2018-07-05 18:51:23 +0000

    Restructure 15-binary_tree_is_full.c

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -13,10 +13,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 
 	is_full = 1;
-	if (tree->left != NULL)
-		is_full = binary_tree_is_full(tree->left);
-	if (tree->right != NULL)
-		is_full = binary_tree_is_full(tree->right);
 
 	if (((tree->left == NULL && tree->right == NULL) ||
 	     (tree->left != NULL && tree->right != NULL)) &&
@@ -24,5 +20,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		is_full = 1;
 	else
 		is_full = 0;
+
+	if (tree->left != NULL)
+		is_full = binary_tree_is_full(tree->left);
+	if (tree->right != NULL)
+		is_full = binary_tree_is_full(tree->right);
+
 	return (is_full);
 }
